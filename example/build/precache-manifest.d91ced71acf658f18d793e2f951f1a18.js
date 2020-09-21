197561e39a54e288b7edaf4b0b31023dbe3c2fcc
Hasan <mhasan.kzl@gmail.com>
2020-09-21 23:14:45 +0300

    build error solved

---
+++ example/build/precache-manifest.d91ced71acf658f18d793e2f951f1a18.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "051e68401ea82e8148041b809e3e8a4f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c8c650c1e3fd12b5d2e6",
+    "url": "/static/css/main.f724fda5.chunk.css"
+  },
+  {
+    "revision": "f5c39694c353891343d7",
+    "url": "/static/js/2.c3b959e1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.c3b959e1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c8c650c1e3fd12b5d2e6",
+    "url": "/static/js/main.152f5221.chunk.js"
+  },
+  {
+    "revision": "bea266e306e08d97e8c2",
+    "url": "/static/js/runtime-main.9d7b5c1d.js"
+  }
+]);
--- example/build/service-worker.js
+++ example/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/reactweathercard/precache-manifest.39b2b13c933ceaa6ca07264b8e79dae3.js"
+  "/precache-manifest.d91ced71acf658f18d793e2f951f1a18.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/reactweathercard/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
